5a387b048ead45e3c567dd442640da27fd2ae9f3
sidepixel73 <asusgamens@gmail.com>
2023-02-15 10:07:52 +0500

    added icons and russians

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function langTextChange(language) {
     let langText = document.getElementById("langText");
     if (language == 'english') {
         langText.innerHTML = "Choose Language";
+        document.title = "Choose Language";
     } else if (language == 'russian') {
-        langText.innerHTML = "Выберите язык"
+        langText.innerHTML = "Выберите язык";
+        document.title = "Выберите язык";
     }
 }
